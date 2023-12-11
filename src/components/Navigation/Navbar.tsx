4b568725a56beeba61f9b072b1307fcfa23fc7bc
Txreq <txreqb2w@duck.com>
2023-12-11 21:35:46 +0100

    fix: `/` hydration error (nested `<Link />`)

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -7,11 +7,9 @@ export default function Navbar(props: { className?: string }) {
     <header className={props.className}>
       <nav className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
-          <div className="flex-1 md:flex md:items-center md:gap-12">
-            <a className="block text-teal-600" href="/">
-              <span className="sr-only">Home</span>
-              <Logo />
-            </a>
+          <div className="flex-1 md:flex md:items-center md:gap-12"></div>
+            <span className="sr-only">Home</span>
+            <Logo />
           </div>
 
           <div className="md:flex md:items-center md:gap-12">
