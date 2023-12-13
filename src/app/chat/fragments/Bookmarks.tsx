2d0b6f565940d3786d178fecee24919eb451c7cd
Txreq <txreqb2w@duck.com>
2023-12-13 10:11:18 +0100

    feat: bookmarks side bar open/close

--- src/app/chat/fragments/Bookmarks.tsx
+++ src/app/chat/fragments/Bookmarks.tsx
@@ -5,50 +5,102 @@ import { useState } from "react";
 
 // icons
 import {
+  LuArrowLeft,
+  LuArrowRight,
   LuFileEdit,
   LuMenu,
   LuMoreHorizontal,
   LuPencil,
+  LuPencilLine,
+  LuSettings,
   LuX,
 } from "react-icons/lu";
 import { api } from "@/trpc/react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/Display";
+
+import type { User } from "next-auth";
 
 interface HistoryProps {
   className?: string;
+  user: User;
 }
 
-const Bookmarks: React.FC<HistoryProps> = ({ className }) => {
+const Bookmarks: React.FC<HistoryProps> = ({ className, user }) => {
   const bookmark = api.bookmark.list.useQuery();
-  const [isOpen, setOpen] = useState<boolean>(false);
+  const [isOpen, onOpen] = useState<boolean>(false);
 
   return (
     <>
-      <div className="hidden p-4 md:visible">
-        <Button
-          onClick={() => setOpen(true)}
-          variant="secondary"
-          className="p-2 text-lg"
-        >
-          <LuMenu />
-        </Button>
-      </div>
-      <aside
+      <div
         className={cn(
-          "h-full bg-secondary p-4 pt-0 text-accent-foreground",
-          className,
+          "absolute left-0 top-1/2 -translate-y-1/2 p-2 transition-all duration-200",
+          isOpen ? "ml-[250px]" : "ml-0",
         )}
       >
-        <ul>
-          {bookmark.data?.map((bookmark) => (
-            <li className="group relative cursor-pointer truncate rounded-lg bg-secondary px-4 py-3 duration-200 hover:bg-white/[.1] hover:shadow-lg">
-              <span>{bookmark.title}</span>
-              <span className="invisible absolute right-2 top-1/2 float-right -translate-y-1/2 rounded-lg bg-accent p-2 shadow-xl transition-all duration-200 hover:bg-background group-hover:visible">
-                <LuMoreHorizontal />
-              </span>
-            </li>
-          ))}
-        </ul>
-      </aside>
+        <Button
+          variant="ghost"
+          className="px-2 text-lg"
+          onClick={() => onOpen((prev) => !prev)}
+        >
+          <span>{isOpen ? <LuArrowLeft /> : <LuArrowRight />}</span>
+        </Button>
+      </div>
+      <div
+        className={cn(
+          "overflow-x-hidden transition-all duration-200",
+          isOpen ? "w-[250px]" : "w-0",
+        )}
+      >
+        <div className="relative z-50 h-full w-full bg-secondary">
+          <div className="grid h-full w-full grid-cols-1 gap-y-2">
+            <div className="row-span-full row-start-1 row-end-1 w-full p-4">
+              <Button
+                className="inline-flex w-full justify-between"
+                variant="outline"
+              >
+                <div className="inline-flex items-center gap-x-2">
+                  <span>New chat</span>
+                </div>
+                <LuPencilLine />
+              </Button>
+            </div>
+            <div
+              style={{
+                gridRow: "span 10 / span 10",
+              }}
+            >
+              <aside
+                className={cn(
+                  "no-scrollbar h-full overflow-y-scroll bg-secondary p-4 pt-0 text-accent-foreground",
+                  className,
+                )}
+              >
+                <ul>
+                  {bookmark.data?.map((bookmark) => (
+                    <li className="group relative cursor-pointer truncate rounded-lg bg-secondary px-4 py-3 duration-200 hover:bg-white/[.1] hover:shadow-lg">
+                      <span>{bookmark.title}</span>
+                      <span className="invisible absolute right-2 top-1/2 float-right -translate-y-1/2 rounded-lg bg-accent p-2 shadow-xl transition-all duration-200 hover:bg-background group-hover:visible">
+                        <LuMoreHorizontal />
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </aside>
+            </div>
+            <div className="row-span-1">
+              <div className="inline-flex h-full w-full items-center justify-between p-4 py-0">
+                <Avatar>
+                  <AvatarImage src={user.image ?? ""} alt={`@${user.name}`} />
+                  <AvatarFallback>🫵</AvatarFallback>
+                </Avatar>
+                <Button variant="outline" className="px-2">
+                  <LuSettings />
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,48 +1,10 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/Display";
-import { Button } from "@/components/Form";
-import { Conversation, Bookmarks } from "./fragments";
-
+import { Bookmarks, Conversation } from "./fragments";
 import { withAuth } from "@/hocs";
-import { LuPencilLine, LuSettings } from "react-icons/lu";
 
 export default withAuth(({ user }) => {
   return (
     <div className="flex h-screen w-screen overflow-hidden">
-      <div className="relative z-50 h-full w-[250px] bg-secondary">
-        <div className="grid h-full w-full grid-cols-1 gap-y-2">
-          <div className="row-span-full row-start-1 row-end-1 w-full p-4">
-            <Button
-              className="inline-flex w-full justify-between"
-              variant="outline"
-            >
-              <div className="inline-flex items-center gap-x-2">
-                <span>New chat</span>
-              </div>
-              <LuPencilLine />
-            </Button>
-          </div>
-          <div
-            style={{
-              gridRow: "span 10 / span 10",
-            }}
-          >
-            <Bookmarks className="no-scrollbar h-full overflow-y-scroll" />
-          </div>
-          <div className="row-span-1">
-            <div className="inline-flex h-full w-full items-center justify-between p-4 py-0">
-              <Avatar>
-                <AvatarImage src={user.image ?? ""} alt={`@${user.name}`} />
-                <AvatarFallback>🫵</AvatarFallback>
-              </Avatar>
-              <Button variant="outline" className="px-2">
-                <LuSettings />
-              </Button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* middle */}
+      <Bookmarks user={user} />
       <div className="flex-1">
         <div className="max-w-screen-full mx-auto h-full">
           <Conversation user={user} />
